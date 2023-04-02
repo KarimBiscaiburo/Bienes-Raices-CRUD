4afcc0961816d101307744bdedbe6fbcb65d643c
Karim Biscaiburo <kbiscaiburo@gmail.com>
2023-04-02 15:21:26 -0300

    add server connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dataRoutes from "./routes/dataRoutes.js";
 
+import { PORT } from "./config.js";
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,5 @@ app.use(express.json());
 
 app.use(dataRoutes);
 
-app.listen(4000);
-console.log(`Server is listening on port 4000`);
+app.listen(PORT);
+console.log(`Server is listening on port ${PORT}`);
